bb75817796fc3b27a0bd6f0774409fba70e41040
Mdevpro78 <mdevpro.2020@gmail.com>
2024-10-14 16:36:26 +0330

    feat(src.conversion): add required fastapi and pydantic modules

---
+++ src/conversion/__init__.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel as BaseModel
+from pydantic import field_validator as field_validator
+from pydantic import  ValidationError as ValidationError
+from pydantic import  ConfigDict as ConfigDict
+from fastapi import APIRouter as APIRouter
+from fastapi import HTTPException as HTTPException
